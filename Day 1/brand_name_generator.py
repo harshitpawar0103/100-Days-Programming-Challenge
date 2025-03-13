8a5c30a0270e4c73cf3b804f6d11ce1f150d71b5
harshitpawar0103 <harshitpawar0103@gmail.com>
2025-03-14 01:43:13 +0530

    Day 1

---
+++ Day 1/brand_name_generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Brand Name Genereator")
+city = input("What's the name of the city you grew up in ? \n")
+pet = input("What's your frst pet's name ? \n")
+
+print("Your brand name could be "+city + " " + pet)
---
+++ Day 1/string_manipulation.py
@@ -0,0 +1,9 @@
+# String Concatenation
+print("Hello" +" " +"Angela")
+
+# User Input
+name = input("What is your name ? : ")
+print("Hello " + name + "!")
+
+# use of len() function
+print(f"No of characters in your name is : {len(name)}")
