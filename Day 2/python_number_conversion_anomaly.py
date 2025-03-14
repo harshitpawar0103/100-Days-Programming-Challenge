7a8fcc91aecc7f3a4d7003ab2ac71072e5a1901d
harshitpawar0103 <harshitpawar0103@gmail.com>
2025-03-14 12:28:29 +0530

    re-writted python no conversion anomaly

---
+++ Day 2/python_number_conversion_anomaly.py
@@ -0,0 +1,3 @@
+print(150 * 1.12)
+
+# prints 168.00000000000003 instead of 168 because python first converts numbers in binary, then calculates and 1.12, when converted in binary has infinite repeating fraction.
